784d33737399b82460d4390a987b932cb4bfea2b
narendra2000 <46496466+narendra2000@users.noreply.github.com>
2020-10-03 17:01:29 +0530

    Update Nesting_depth.cpp

--- Nesting_depth.cpp
+++ Nesting_depth.cpp
@@ -6,8 +6,22 @@ using namespace std;
 #define endl "\n";
 int no = 1;
 void D(){
-	
-	string s ;cin>>s;
+
+}
+
+int main(int argc, char const *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cout.precision(0);
+	cout<<fixed;
+
+	ll n , m,k ,pt(0),ct(0) ,x,y; 
+	ll t =1; 
+	cin>>t;
+	while(t--){
+		string s ;cin>>s;
 	string res;
 	int  n = s.length();
 
@@ -31,21 +45,6 @@ void D(){
 	while(prev--) res+=')';
 	cout<<"Case #"<<no<<": "<<res<<endl;
 	no++;
-}
-
-int main(int argc, char const *argv[])
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	cout.precision(0);
-	cout<<fixed;
-
-	ll n , m,k ,pt(0),ct(0) ,x,y; 
-	ll t =1; 
-	cin>>t;
-	while(t--){
-		D();
 	}
 
 }	
